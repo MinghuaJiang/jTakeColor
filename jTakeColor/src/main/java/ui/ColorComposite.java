8416e57eacca327755e01545b82496949b8a2739
MinghuaJiang <61221630@qq.com>
2015-02-12 18:34:55 +0800

    Create ColorComposite.java

---
+++ jTakeColor/src/main/java/ui/ColorComposite.java
@@ -0,0 +1,98 @@
+package ui.composite;
+
+import java.util.Arrays;
+
+import model.CaptureType;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+public class ColorComposite extends Composite{
+
+	private Label colorBox;
+	private Label positionText;
+	private Label colorText;
+	private Combo typeCombo;
+
+	public ColorComposite(Composite parent, int style){
+		super(parent,style);
+		GridData gridData = new GridData();
+		gridData.widthHint = 100;
+		gridData.heightHint = 200;
+		this.setLayoutData(gridData);
+		this.setLayout(new FormLayout());
+		FormData formData = new FormData();
+		formData.left = new FormAttachment(0, 10);
+		formData.right = new FormAttachment(100, -10);
+		formData.top = new FormAttachment(0, 10);
+		formData.bottom = new FormAttachment(50, -10);
+		colorBox = new Label(this, SWT.NONE);
+		colorBox.setLayoutData(formData);
+		colorBox.addPaintListener(new PaintListener() {
+			public void paintControl(PaintEvent e) {
+				Label capture = (Label) e.widget;
+				e.gc.drawRectangle(0, 0, capture.getSize().x - 1, capture
+						.getSize().y - 1);
+			}
+		});
+		positionText = new Label(this, SWT.NONE);
+		formData = new FormData();
+		formData.left = new FormAttachment(0, 10);
+		formData.right = new FormAttachment(100, -10);
+		formData.top = new FormAttachment(colorBox, 10);
+
+		positionText.setLayoutData(formData);
+		colorText = new Label(this, SWT.NONE);
+		formData = new FormData();
+		formData.left = new FormAttachment(0, 10);
+		formData.right = new FormAttachment(100, -10);
+		formData.top = new FormAttachment(positionText, 15);
+		formData.bottom = new FormAttachment(80, -10);
+		colorText.setLayoutData(formData);
+		typeCombo = new Combo(this, SWT.READ_ONLY);
+		formData = new FormData();
+		formData.left = new FormAttachment(0, 10);
+		formData.right = new FormAttachment(100, -10);
+		formData.top = new FormAttachment(colorText, 10);
+		formData.bottom = new FormAttachment(100, -10);
+		typeCombo.setLayoutData(formData);
+		typeCombo.setItems(Lists.transform(Arrays.asList(CaptureType.values()), new Function<CaptureType,String>(){
+			@Override
+			public String apply(CaptureType input) {
+				return input.name();
+			}
+			
+		}).toArray(new String[0]));
+		typeCombo.select(0);
+		
+	}
+
+	public Label getColorBox() {
+		return colorBox;
+	}
+
+	public Label getPositionText() {
+		return positionText;
+	}
+
+	public Label getColorText() {
+		return colorText;
+	}
+
+	public Combo getTypeCombo() {
+		return typeCombo;
+	}
+	
+}
