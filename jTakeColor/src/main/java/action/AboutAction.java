61df8be037e0c3a3a50c8c5563b815194f76299f
MinghuaJiang <61221630@qq.com>
2015-02-12 18:26:48 +0800

    new file
    action

---
+++ jTakeColor/src/main/java/action/AboutAction.java
@@ -0,0 +1,19 @@
+package action;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.swt.SWT;
+
+import dialog.AboutDialog;
+
+public class AboutAction extends Action {
+	public AboutAction() {
+		super();
+		this.setText("About");
+		this.setAccelerator(SWT.CTRL + 'A');
+		this.setToolTipText("About");
+	}
+
+	public void run() {
+		AboutDialog.showAboutMessageBox();
+	}
+}
