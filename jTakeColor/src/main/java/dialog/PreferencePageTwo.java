6a38ba644801c7af43d1e6423566b2427c7f34c6
MinghuaJiang <61221630@qq.com>
2015-02-12 18:30:56 +0800

    Update PreferencePageTwo.java

--- jTakeColor/src/main/java/dialog/PreferencePageTwo.java
+++ jTakeColor/src/main/java/dialog/PreferencePageTwo.java
@@ -8,9 +8,9 @@ public class PreferencePageTwo extends FieldEditorPreferencePage {
 	@Override
 	protected void createFieldEditors() {
 		// TODO Auto-generated method stub
-		RadioGroupFieldEditor rgfe = new RadioGroupFieldEditor("amplify", "·Å´ó",
+		RadioGroupFieldEditor rgfe = new RadioGroupFieldEditor("amplify", "Ratio",
 				3,
-				new String[][] { { "2±¶", "2" }, { "4±¶", "4" }, { "6±¶", "6" } },
+				new String[][] { { "2", "2" }, { "4", "4" }, { "6", "6" } },
 				getFieldEditorParent(), true);
 		addField(rgfe);
 	}
