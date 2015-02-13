10f6569a06a296eeb240a6470843ab7e09ff70ea
MinghuaJiang <61221630@qq.com>
2015-02-13 18:01:10 +0800

    Update Preference.java

--- jTakeColor/src/main/java/dialog/Preference.java
+++ jTakeColor/src/main/java/dialog/Preference.java
@@ -1,6 +1,7 @@
 package dialog;
 
 import org.eclipse.jface.preference.*;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Display;
 
 import util.PreferenceUtil;
@@ -9,19 +10,18 @@ public class Preference {
 	public static void showPreference() {
 		Display display = Display.getCurrent();
 		PreferenceManager mgr = new PreferenceManager();
-		PreferenceNode hotkey = new PreferenceNode("hotkey", "Hot Key", null,
-				PreferencePageOne.class.getName());
 		PreferenceNode amplified = new PreferenceNode("amplify", "Amplify", null,
-				PreferencePageTwo.class.getName());
-		mgr.addToRoot(hotkey);
+				AmplifyPage.class.getName());
 		mgr.addToRoot(amplified);
 		PreferenceUtil pUtil = PreferenceUtil.getInstance();
 		pUtil.load();
 		PreferenceDialog pDialog = new PreferenceDialog(
 				display.getActiveShell(), mgr);
 		pDialog.setPreferenceStore(pUtil.getPreferenceStore());
-		pDialog.open();
-		pUtil.save();
-
+		int code = pDialog.open();
+		if(code == Window.OK){
+			pUtil.save();
+		}
 	}
 }
+
